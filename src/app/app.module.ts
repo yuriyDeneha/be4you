cb336aa5163df069d28e3dd258da1d3e8cfbff04
yuriyDeneha <yuriy.deneha@techmagic.co>
2017-10-31 11:55:12 +0200

    Added some sonfig for firebase, installed material design library, created basic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {environment} from '../environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule} from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {environment} from '../environments/environment';
 import { HomeComponent } from './components/home/home.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { ContentComponent } from './components/content/content.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { AuthComponent } from './components/auth/auth.component';
+
+//Material design modules
+import { MatButtonModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TopBarComponent,
-    ContentComponent
+    ContentComponent,
+    ShopComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    // material elements
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
