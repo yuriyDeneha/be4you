f3f2a2b4c2555dc9742980c4bfcaf6ac9dd7234f
yuriyDeneha <yuriy.deneha@techmagic.co>
2017-11-09 12:25:52 +0200

    fixed order block

--- src/app/components/home/quick-order/choose-flowers/choose-flowers.component.ts
+++ src/app/components/home/quick-order/choose-flowers/choose-flowers.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-choose-flowers',
   templateUrl: './choose-flowers.component.html',
-  styleUrls: ['./choose-flowers.component.scss']
+  styleUrls: ['./choose-flowers.component.sass']
 })
 export class ChooseFlowersComponent implements OnInit {
 
